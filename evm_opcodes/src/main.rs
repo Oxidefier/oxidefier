a60ff219d546a7e84626820b43742dbfe602aeb4
Guillaume Claret <dev@clarus.me>
2025-04-22 14:32:05 +0200

    opcodes: add memory as a parameter

--- evm_opcodes/src/main.rs
+++ evm_opcodes/src/main.rs
@@ -4,160 +4,6 @@ use core::cmp::Ordering;
 mod i256;
 mod macros;
 
-enum ReturnOrRevert {
-    Return { start: U256, length: U256 },
-    Revert { start: U256, length: U256 },
-}
-
-type YulOutput<A> = Result<A, ReturnOrRevert>;
-
-// Pure opcodes
-
-fn add(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(x.wrapping_add(y))
-}
-
-fn sub(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(x.wrapping_sub(y))
-}
-
-fn mul(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(x.wrapping_mul(y))
-}
-
-fn div(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(if y == U256::ZERO {
-        U256::ZERO
-    } else {
-        x.wrapping_div(y)
-    })
-}
-
-fn sdiv(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(i256::i256_div(x, y))
-}
-
-fn mod_(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(if y == U256::ZERO {
-        U256::ZERO
-    } else {
-        x.wrapping_rem(y)
-    })
-}
-
-fn smod(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(i256::i256_mod(x, y))
-}
-
-fn exp(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(x.pow(y))
-}
-
-fn not(x: U256) -> YulOutput<U256> {
-    Ok(!x)
-}
-
-fn lt(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(U256::from(x < y))
-}
-
-fn gt(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(U256::from(x > y))
-}
-
-fn slt(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(U256::from(i256::i256_cmp(&x, &y) == Ordering::Less))
-}
-
-fn sgt(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(U256::from(i256::i256_cmp(&x, &y) == Ordering::Greater))
-}
-
-fn eq(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(U256::from(x == y))
-}
-
-fn iszero(x: U256) -> YulOutput<U256> {
-    Ok(U256::from(x.is_zero()))
-}
-
-fn and(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(x & y)
-}
-
-fn or(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(x | y)
-}
-
-fn xor(x: U256, y: U256) -> YulOutput<U256> {
-    Ok(x ^ y)
-}
-
-fn byte(op1: U256, op2: U256) -> YulOutput<U256> {
-    let o1 = as_usize_saturated!(op1);
-    if o1 < 32 {
-        // `31 - o1` because `byte` returns LE, while we want BE
-        Ok(U256::from(op2.byte(31 - o1)))
-    } else {
-        Ok(U256::ZERO)
-    }
-}
-
-fn shl(op1: U256, op2: U256) -> YulOutput<U256> {
-    let shift = as_usize_saturated!(op1);
-    if shift < 256 {
-        Ok(op2 << shift)
-    } else {
-        Ok(U256::ZERO)
-    }
-}
-
-fn shr(op1: U256, op2: U256) -> YulOutput<U256> {
-    let shift = as_usize_saturated!(op1);
-    if shift < 256 {
-        Ok(op2 >> shift)
-    } else {
-        Ok(U256::ZERO)
-    }
-}
-
-fn sar(op1: U256, op2: U256) -> YulOutput<U256> {
-    let shift = as_usize_saturated!(op1);
-    if shift < 256 {
-        Ok(op2.arithmetic_shr(shift))
-    } else if op2.bit(255) {
-        Ok(U256::MAX)
-    } else {
-        Ok(U256::ZERO)
-    }
-}
-
-fn addmod(op1: U256, op2: U256, op3: U256) -> YulOutput<U256> {
-    Ok(op1.add_mod(op2, op3))
-}
-
-fn mulmod(op1: U256, op2: U256, op3: U256) -> YulOutput<U256> {
-    Ok(op1.mul_mod(op2, op3))
-}
-
-fn signextend(ext: U256, x: U256) -> YulOutput<U256> {
-    // For 31 we also don't need to do anything.
-    if ext < U256::from(31) {
-        let ext = ext.as_limbs()[0];
-        let bit_index = (8 * ext + 7) as usize;
-        let bit = x.bit(bit_index);
-        let mask = (U256::from(1) << bit_index) - U256::from(1);
-        if bit {
-            Ok(x | !mask)
-        } else {
-            Ok(x & mask)
-        }
-    } else {
-        Ok(x)
-    }
-}
-
-// Memory opcodes
 #[derive(Debug)]
 struct Memory {
     inner: Vec<u8>,
@@ -200,6 +46,162 @@ impl Memory {
         }
     }
 }
+
+#[derive(Debug)]
+enum ReturnOrRevert {
+    Return { start: U256, length: U256 },
+    Revert { start: U256, length: U256 },
+}
+
+type YulOutput<A> = Result<A, ReturnOrRevert>;
+
+// Pure opcodes
+
+fn add(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(x.wrapping_add(y))
+}
+
+fn sub(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(x.wrapping_sub(y))
+}
+
+fn mul(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(x.wrapping_mul(y))
+}
+
+fn div(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(if y == U256::ZERO {
+        U256::ZERO
+    } else {
+        x.wrapping_div(y)
+    })
+}
+
+fn sdiv(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(i256::i256_div(x, y))
+}
+
+fn mod_(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(if y == U256::ZERO {
+        U256::ZERO
+    } else {
+        x.wrapping_rem(y)
+    })
+}
+
+fn smod(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(i256::i256_mod(x, y))
+}
+
+fn exp(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(x.pow(y))
+}
+
+fn not(_memory: &Memory, x: U256) -> YulOutput<U256> {
+    Ok(!x)
+}
+
+fn lt(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(U256::from(x < y))
+}
+
+fn gt(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(U256::from(x > y))
+}
+
+fn slt(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(U256::from(i256::i256_cmp(&x, &y) == Ordering::Less))
+}
+
+fn sgt(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(U256::from(i256::i256_cmp(&x, &y) == Ordering::Greater))
+}
+
+fn eq(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(U256::from(x == y))
+}
+
+fn iszero(_memory: &Memory, x: U256) -> YulOutput<U256> {
+    Ok(U256::from(x.is_zero()))
+}
+
+fn and(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(x & y)
+}
+
+fn or(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(x | y)
+}
+
+fn xor(_memory: &Memory, x: U256, y: U256) -> YulOutput<U256> {
+    Ok(x ^ y)
+}
+
+fn byte(_memory: &Memory, op1: U256, op2: U256) -> YulOutput<U256> {
+    let o1 = as_usize_saturated!(op1);
+    if o1 < 32 {
+        // `31 - o1` because `byte` returns LE, while we want BE
+        Ok(U256::from(op2.byte(31 - o1)))
+    } else {
+        Ok(U256::ZERO)
+    }
+}
+
+fn shl(_memory: &Memory, op1: U256, op2: U256) -> YulOutput<U256> {
+    let shift = as_usize_saturated!(op1);
+    if shift < 256 {
+        Ok(op2 << shift)
+    } else {
+        Ok(U256::ZERO)
+    }
+}
+
+fn shr(_memory: &Memory, op1: U256, op2: U256) -> YulOutput<U256> {
+    let shift = as_usize_saturated!(op1);
+    if shift < 256 {
+        Ok(op2 >> shift)
+    } else {
+        Ok(U256::ZERO)
+    }
+}
+
+fn sar(_memory: &Memory, op1: U256, op2: U256) -> YulOutput<U256> {
+    let shift = as_usize_saturated!(op1);
+    if shift < 256 {
+        Ok(op2.arithmetic_shr(shift))
+    } else if op2.bit(255) {
+        Ok(U256::MAX)
+    } else {
+        Ok(U256::ZERO)
+    }
+}
+
+fn addmod(_memory: &Memory, op1: U256, op2: U256, op3: U256) -> YulOutput<U256> {
+    Ok(op1.add_mod(op2, op3))
+}
+
+fn mulmod(_memory: &Memory, op1: U256, op2: U256, op3: U256) -> YulOutput<U256> {
+    Ok(op1.mul_mod(op2, op3))
+}
+
+fn signextend(_memory: &Memory, ext: U256, x: U256) -> YulOutput<U256> {
+    // For 31 we also don't need to do anything.
+    if ext < U256::from(31) {
+        let ext = ext.as_limbs()[0];
+        let bit_index = (8 * ext + 7) as usize;
+        let bit = x.bit(bit_index);
+        let mask = (U256::from(1) << bit_index) - U256::from(1);
+        if bit {
+            Ok(x | !mask)
+        } else {
+            Ok(x & mask)
+        }
+    } else {
+        Ok(x)
+    }
+}
+
+// Memory opcodes
 
 fn mload(memory: &Memory, address: U256) -> YulOutput<U256> {
     let bytes: Vec<u8> = memory.load(address, U256::from(32));
